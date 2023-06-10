7454494e7d5af5b5ba407163cd49befc896c8f08
Jordi Sabat <jsabat@gmail.com>
2023-06-10 15:39:16 +0200

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <div>Optimise your healthspan</div>;
+};
+
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("no container to render to");
+}
+const root = createRoot(container);
+root.render(<App />);
