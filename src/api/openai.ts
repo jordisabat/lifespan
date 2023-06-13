7e82f7f04e46062d23d04ffcb98aa44b4f77b874
Jordi Sabat <jsabat@gmail.com>
2023-06-13 10:55:08 +0200

    reafactor: remove OPENAI API KEY

--- src/api/openai.ts
+++ src/api/openai.ts
@@ -6,7 +6,7 @@ export const fetchAdvice = async (user: UserType): Promise<string> => {
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
-        Authorization: `Bearer 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`,
+        Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
